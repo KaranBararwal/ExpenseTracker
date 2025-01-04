25db87447168e81b051099ee2ad5f23bc07ccfe1
Karan <159271329+KaranBararwal@users.noreply.github.com>
2025-01-04 14:59:57 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,13 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware();
 
+// Configuring middleware to match routes
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    // Exclude static files and Next.js internals
+    '/((?!_next/static|_next/image|favicon.ico|static).*)',
+    // Always include API routes
+    '/api/(.*)',
   ],
 };
